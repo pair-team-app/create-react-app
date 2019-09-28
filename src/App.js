415d7959cb2881f3c85fc7459326026c9e9caaed
mtholcombe <matt@designengine.ai>
2019-09-28 14:03:11 -0700

    adding data tags

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import logo from './spectrum-logo.png';
 import './spectrum.css';
@@ -5,42 +6,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" data-component-name="Logo" />
 
 	      <div className="react-spectrum-provider spectrum spectrum--light spectrum--medium">
 		      <div className="row">
-			      <button className="spectrum-Button spectrum-Button--cta"><span className="spectrum-Button-label">React</span>
-			      </button>
-			      <button className="spectrum-Button spectrum-Button--cta is-disabled" disabled="">
-				      <span className="spectrum-Button-label">React</span></button>
+			      <button className="spectrum-Button spectrum-Button--cta" data-component-name="CTA Button"><span className="spectrum-Button-label">React</span></button>
+			      <button className="spectrum-Button spectrum-Button--cta is-disabled" disabled="" data-component-name="CTA Button Disabled"><span className="spectrum-Button-label">React</span></button>
 		      </div>
 
 		      <div className="row">
-			      <button className="spectrum-Button spectrum-Button--primary">
-				      <span className="spectrum-Button-label">React</span></button>
-			      <button className="spectrum-Button spectrum-Button--primary is-disabled" disabled="">
-				      <span className="spectrum-Button-label">React</span></button>
+			      <button className="spectrum-Button spectrum-Button--primary" data-component-name="Primary Button"><span className="spectrum-Button-label">React</span></button>
+			      <button className="spectrum-Button spectrum-Button--primary is-disabled" disabled="" data-component-name="Primary Button Disabled"><span className="spectrum-Button-label">React</span></button>
 		      </div>
 
 		      <div className="row">
-			      <button className="spectrum-Button spectrum-Button--secondary">
-				      <span className="spectrum-Button-label">React</span></button>
-			      <button className="spectrum-Button spectrum-Button--secondary is-disabled" disabled="">
-				      <span className="spectrum-Button-label">React</span></button>
+			      <button className="spectrum-Button spectrum-Button--secondary" data-component-name="Secondary Button"><span className="spectrum-Button-label">React</span></button>
+			      <button className="spectrum-Button spectrum-Button--secondary is-disabled" disabled="" data-component-name="Secondary Button Disabled"><span className="spectrum-Button-label">React</span></button>
 		      </div>
 
 		      <div className="row">
-			      <button className="spectrum-Button spectrum-Button--warning">
-				      <span className="spectrum-Button-label">React</span></button>
-			      <button className="spectrum-Button spectrum-Button--warning is-disabled" disabled="">
-				      <span className="spectrum-Button-label">React</span></button>
+			      <button className="spectrum-Button spectrum-Button--warning" data-component-name="Warning Button"><span className="spectrum-Button-label">React</span></button>
+			      <button className="spectrum-Button spectrum-Button--warning is-disabled" disabled="" data-component-name="Warning Button Disabled"><span className="spectrum-Button-label">React</span></button>
 		      </div>
 	      </div>
 
 	      <div className="row">
-		      <table className="spectrum-Table">
+		      <table className="spectrum-Table" data-component-name="Table">
 			      <thead className="spectrum-Table-head" style={{color: 'color: rgb(110, 110, 110);'}}>
 			      <tr>
 				      <th className="spectrum-Table-headCell">Pet Name</th>
